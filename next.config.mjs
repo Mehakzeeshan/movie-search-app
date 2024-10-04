99d8e5a0d9e5e1bbfa299845ace759d5380fb8d2
Mehak Noor Mohammad <159150549+Mehakzeeshan@users.noreply.github.com>
2024-10-05 04:04:41 +0500

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      domains: ['m.media-amazon.com'],
+    },
+  };
+  
+  export default nextConfig;
+  
+  
+  
